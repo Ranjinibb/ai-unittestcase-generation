9dc6fa6c848c1d354f55f0caa626c332af1d4e50
rbearatt <ranjinibb@gmail.com>
2026-01-30 13:45:46 +0100

    changes

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -10,4 +10,9 @@ public class HelloWorldController {
     public String sendGreetings() {
         return "Hello, World!";
     }
+
+    @GetMapping("/hello1")
+    public String sendGreetings1() {
+        return "Hello, World!";
+    }
 }
