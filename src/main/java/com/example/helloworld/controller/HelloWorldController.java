3eaeeba5c6c261177b7adb2d0c35fe7c49c62a33
rbearatt <ranjinibb@gmail.com>
2026-02-05 10:23:15 +0100

    final_code

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -16,9 +16,5 @@ public class HelloWorldController {
         return "Hello, World!";
     }
 
-    @GetMapping("/helloWorldTwo")
-    public String helloWorldTwo() {
-        return "Hello, World!";
-    }
 
 }
