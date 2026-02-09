f5923a34e9c0672db03fbbfccbbb0644f117a8b4
rbearatt <ranjinibb@gmail.com>
2026-02-09 10:43:50 +0100

    unitest

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -26,10 +26,4 @@ public class HelloWorldController {
         return "Hello, World!";
     }
 
-    @GetMapping("/helloWorldFour")
-    public String helloWorldFour() {
-        return "Hello, World!";
-    }
-
-
 }
